e998a879c726795798d16166010a8ab005dd3458
ap0calypse9 <logantrek@gmail.com>
2025-09-10 01:34:06 +0530

    refactor text extraction to use list comprehension for improved readability

--- load_doc.py
+++ load_doc.py
@@ -2,10 +2,7 @@ from pypdf import PdfReader
 
 
 reader = PdfReader("test.pdf")  
-text = ""
-
-for page in reader.pages:
-    text += page.extract_text()
+text = "".join([page.extract_text() for page in reader.pages])
 
 print("=== Raw Document Text ===")
 print(text[:1000])   # printing first 1000 chars only
